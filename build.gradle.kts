a66133c8d4a458610d6d550e59d6c36b0932af5d
Malfrador <malfrador@gmail.com>
2025-07-16 23:20:56 +0200

    Model changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,23 +3,13 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 repositories {
     mavenLocal()
     maven("https://repo.erethon.de/snapshots/")
-    maven("https://repo.unnamed.team/repository/unnamed-public/")
-    maven("https://repo.dmulloy2.net/repository/public/")
-    maven("https://oss.sonatype.org/content/groups/public/")
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-    maven("https://repo.aikar.co/content/groups/aikar")
-    maven("https://repo.md-5.net/content/repositories/releases/")
-    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-    maven("https://jitpack.io")
 }
 
 plugins {
     `java-library`
     `maven-publish`
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
-    id("xyz.jpenilla.run-paper") version "1.0.6" // Adds runServer and runMojangMappedServer tasks for testing
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
     id("io.github.goooler.shadow") version "8.1.5"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
 }
@@ -29,7 +19,6 @@ version = "1.0.1-SNAPSHOT"
 description = "Mob and NPC plugin for Erethon"
 
 java {
-    // Configure the java toolchain. This allows Gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
@@ -37,20 +26,16 @@ val papyrusVersion = "1.21.7-R0.1-SNAPSHOT"
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 dependencies {
-    paperweightDevelopmentBundle("de.erethon.papyrus", "dev-bundle", papyrusVersion) { isChanging = true }
-    implementation("de.erethon:bedrock:1.4.0") { isTransitive = false }
-    compileOnly("de.erethon.questsxl:QuestsXL:1.0.0-SNAPSHOT")
+    paperweightDevelopmentBundle("de.erethon.papyrus", "dev-bundle", papyrusVersion)
 
-    implementation("net.worldseed.multipart", "WorldSeedEntityEngine", "12.1")
-    implementation("org.zeroturnaround:zt-zip:1.8") // Outdated, but newer versions seem to have entirely different API annoyingly
+    implementation("de.erethon:bedrock:1.4.0") { isTransitive = false }
+    implementation("de.erethon", "Daedalus", "12.1")
 
     compileOnly("de.erethon.hephaestus:Hephaestus:1.0-SNAPSHOT")
-
+    compileOnly("de.erethon.questsxl:QuestsXL:1.0.0-SNAPSHOT")
 }
 
 tasks {
-    // Configure reobfJar to run when invoking the build task
-
     runServer {
         if (!project.buildDir.exists()) {
             project.buildDir.mkdir()
@@ -93,15 +78,10 @@ tasks {
     shadowJar {
         dependencies {
             include(dependency("de.erethon:bedrock:.*"))
-            include(dependency("net.worldseed.multipart:WorldSeedEntityEngine:12.1"))
-            // Make sure those match the WorldSeedEntityEngine versions
-            include(dependency("org.zeroturnaround:zt-zip:1.8"))
-            include(dependency("javax.json:javax.json-api:1.1.4"))
-            include(dependency("org.glassfish:javax.json:1.1.4"))
-            include(dependency("dev.hollowcube:mql:1.0.1"))
         }
         relocate("de.erethon.bedrock", "de.erethon.aether.bedrock")
     }
+
     bukkit {
         main = "de.erethon.aether.Aether"
         apiVersion = "1.21"
@@ -135,7 +115,6 @@ tasks.register<Copy>("deployToSharedServer") {
     from(layout.buildDirectory.file("libs/Aether-$version-all.jar"))
     into("C:\\Dev\\Erethon\\plugins")
 }
-
 
 publishing {
     repositories {
