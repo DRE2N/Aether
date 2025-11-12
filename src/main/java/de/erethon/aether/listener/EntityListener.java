d5fd4ec9c09d85ebd321e904f06b0badbc5aa236
Malfrador <malfrador@gmail.com>
2025-11-12 17:26:54 +0100

    Also prevent knockback, fixes DEV-107

--- src/main/java/de/erethon/aether/listener/EntityListener.java
+++ src/main/java/de/erethon/aether/listener/EntityListener.java
@@ -1,14 +1,17 @@
 package de.erethon.aether.listener;
 
+import com.destroystokyo.paper.event.entity.EntityKnockbackByEntityEvent;
 import de.erethon.aether.Aether;
 import de.erethon.aether.creature.AetherBaseMob;
 import de.erethon.papyrus.entities.CraftCustomMob;
 import de.erethon.spellbook.api.SpellEffectAddEvent;
+import io.papermc.paper.event.entity.EntityKnockbackEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class EntityListener implements Listener {
 
@@ -29,6 +32,24 @@ public class EntityListener implements Listener {
             }
             if (aetherMob.getData().isInvulnerableToPlayers() && event.getEffect().getCaster() instanceof Player) {
                 event.setCancelled(true);;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityKnockbackEvent event) {
+        if (event.getEntity() instanceof CraftCustomMob mob && mob.getHandle() instanceof AetherBaseMob aetherMob) {
+            if (aetherMob.getData().isInvulnerable()) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityKnockbackByEntityEvent event) {
+        if (event.getEntity() instanceof CraftCustomMob mob && mob.getHandle() instanceof AetherBaseMob aetherMob) {
+            if (aetherMob.getData().isInvulnerableToPlayers() && event.getHitBy() instanceof Player) {
+                event.setCancelled(true);
             }
         }
     }
